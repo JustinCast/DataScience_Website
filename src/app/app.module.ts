14cc2473f200999cefb8b283be3ff2f828bf9048
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-04-22 13:32:00 -0600

    Routing Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,27 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ROUTES } from './app.routing';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   schemas: [ NO_ERRORS_SCHEMA ],
---
+++ src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+export const ROUTES : Routes = [
+    {path: '', redirectTo:'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'home', component: HomeComponent}
+]
