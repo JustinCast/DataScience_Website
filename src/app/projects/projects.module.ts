a6eda926a35f453d8483ca9be63b6346f89f5177
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-04-24 16:36:51 -0600

    Módulo de Routing de los proyectos

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from './projects/projects.component';
 import { EditProjectComponent } from './edit-project/edit-project.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ProjectsComponent, EditProjectComponent, CreateProjectComponent]
+  declarations: [ProjectsComponent, EditProjectComponent, CreateProjectComponent, ProjectDetailsComponent]
 })
 export class ProjectsModule { }
