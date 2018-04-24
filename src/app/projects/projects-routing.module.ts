65b5b495902d43f2b313f16e46fdd248de3e60d7
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-04-24 16:55:02 -0600

    Se agregaron las rutas hijas del Módulo de Proyectos

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -7,10 +7,14 @@ import { ProjectsComponent } from "./projects/projects.component";
 import { ProjectDetailsComponent } from "./project-details/project-details.component";
 
 const ROUTES: Routes = [
-    { path: 'projects', component: ProjectsComponent },
-    { path: 'project/:id', component: ProjectDetailsComponent },
-    { path: 'project/edit-project/:id', component: EditProjectComponent },
-    { path: 'create-project', component: CreateProjectComponent }
+    { 
+        path: 'projects', component: ProjectsComponent,
+            children: [
+                { path: 'project/:id', component: ProjectDetailsComponent },
+                { path: 'project/edit-project/:id', component: EditProjectComponent },
+                { path: 'create-project', component: CreateProjectComponent }
+            ]
+    },
 ]
 
 @NgModule({
