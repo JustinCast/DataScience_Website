1325b085ba67eb8e944d340db0ba9f3d030e2380
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-04-24 16:07:33 -0600

    Componentes crear, editar Proyectos

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from './projects/projects.component';
+import { EditProjectComponent } from './edit-project/edit-project.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ProjectsComponent]
+  declarations: [ProjectsComponent, EditProjectComponent, CreateProjectComponent]
 })
 export class ProjectsModule { }
