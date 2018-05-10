f0907ab6bc117e7341eedbbae263764accf50998
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-09 18:48:57 -0600

    MembersTableComponent Refactorization

--- src/app/projects/create-project/create-project.component.ts
+++ src/app/projects/create-project/create-project.component.ts
@@ -8,7 +8,7 @@ import { Location } from '@angular/common';
   templateUrl: './create-project.component.html',
   styleUrls: ['./create-project.component.scss']
 })
-export class CreateProjectComponent implements OnInit, AfterViewInit {
+export class CreateProjectComponent implements OnInit {
   investigations = [
     "Investigación 1", 
     "Investigación 2",
@@ -24,17 +24,8 @@ export class CreateProjectComponent implements OnInit, AfterViewInit {
     "Referencia 2",
     "Referencia 3"
   ]
-  displayedColumns = ['name', 'lastName', 'projectCount', 'actions'];
-  addedMembers = []
   addedReferences = []
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
-  }
   constructor(
     private _location: Location
   ) { }
@@ -46,42 +37,8 @@ export class CreateProjectComponent implements OnInit, AfterViewInit {
     this._location.back();
   }
 
-  /**
-   * Set the paginator after the view init since this component will
-   * be able to query its view for the initialized paginator.
-   */
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
-
-  addMember(i) {
-    this.addedMembers.unshift(`${i.name} ${i.lastName}`)
-  }
   addReference(i) {
     this.addedReferences.unshift(i)
   }
 
 }
-
-let ELEMENT_DATA: MemberToDisplay[] = [
-  {name: 'Justin', lastName: "Castillo", projectCount: 3},
-  {name: 'Albert', lastName: "González", projectCount: 2},
-  {name: 'Francisco', lastName: "Corrales", projectCount: 1},
-  {name: 'Alejandro', lastName: "Herrera", projectCount: 5},
-  {name: 'Andres', lastName: "Herrera", projectCount: 5},
-  {name: 'Julio', lastName: "Herrera", projectCount: 5},
-  {name: 'Joshua', lastName: "Herrera", projectCount: 5},
-  {name: 'Kevin', lastName: "Herrera", projectCount: 5},
-  {name: 'Luis', lastName: "Herrera", projectCount: 5},
-  {name: 'Armando', lastName: "Herrera", projectCount: 5},
-  {name: 'Jason', lastName: "Herrera", projectCount: 5},
-  {name: 'Diego', lastName: "Herrera", projectCount: 5},
-  {name: 'Fernando', lastName: "Herrera", projectCount: 5},
-  {name: 'Miguel', lastName: "Herrera", projectCount: 5},
-  {name: 'Jeffry', lastName: "Herrera", projectCount: 5},
-  {name: 'Randald', lastName: "Herrera", projectCount: 5},
-  {name: 'Merelyn', lastName: "Herrera", projectCount: 5},
-  {name: 'Bryan', lastName: "Herrera", projectCount: 5},
-  {name: 'Felipe', lastName: "Herrera", projectCount: 5},
-  {name: 'Josué', lastName: "Herrera", projectCount: 5},
-];
--- src/app/tables/members-table/members-table.component.ts
+++ src/app/tables/members-table/members-table.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MemberToDisplay } from '../../models/MemberToDisplay.interface';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-members-table',
   templateUrl: './members-table.component.html',
   styleUrls: ['./members-table.component.scss']
 })
-export class MembersTableComponent implements OnInit {
-
+export class MembersTableComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  addedMembers = []
+  displayedColumns = ['name', 'lastName', 'projectCount', 'actions'];
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * Set the paginator after the view init since this component will
+   * be able to query its view for the initialized paginator.
+   */
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  addMember(i) {
+    this.addedMembers.unshift(`${i.name} ${i.lastName}`)
+  }
+
 }
+let ELEMENT_DATA: MemberToDisplay[] = [
+  {name: 'Justin', lastName: "Castillo", projectCount: 3},
+  {name: 'Albert', lastName: "González", projectCount: 2},
+  {name: 'Francisco', lastName: "Corrales", projectCount: 1},
+  {name: 'Alejandro', lastName: "Herrera", projectCount: 5},
+  {name: 'Andres', lastName: "Herrera", projectCount: 5},
+  {name: 'Julio', lastName: "Herrera", projectCount: 5},
+  {name: 'Joshua', lastName: "Herrera", projectCount: 5},
+  {name: 'Kevin', lastName: "Herrera", projectCount: 5},
+  {name: 'Luis', lastName: "Herrera", projectCount: 5},
+  {name: 'Armando', lastName: "Herrera", projectCount: 5},
+  {name: 'Jason', lastName: "Herrera", projectCount: 5},
+  {name: 'Diego', lastName: "Herrera", projectCount: 5},
+  {name: 'Fernando', lastName: "Herrera", projectCount: 5},
+  {name: 'Miguel', lastName: "Herrera", projectCount: 5},
+  {name: 'Jeffry', lastName: "Herrera", projectCount: 5},
+  {name: 'Randald', lastName: "Herrera", projectCount: 5},
+  {name: 'Merelyn', lastName: "Herrera", projectCount: 5},
+  {name: 'Bryan', lastName: "Herrera", projectCount: 5},
+  {name: 'Felipe', lastName: "Herrera", projectCount: 5},
+  {name: 'Josué', lastName: "Herrera", projectCount: 5},
+];
