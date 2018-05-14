18cad26fc54ae4eaf62a40729f459b710a2cf9dc
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-14 14:24:20 -0600

    Cambios en el componente CreateReference

--- src/app/references/create-reference/create-reference.component.ts
+++ src/app/references/create-reference/create-reference.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
-
+import { MatDialog } from '@angular/material';
+import { PagesTableComponent } from '../../tables/pages-table/pages-table.component';
+import { CodesTableComponent } from '../../tables/codes-table/codes-table.component';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-create-reference',
   templateUrl: './create-reference.component.html',
@@ -7,10 +10,27 @@ import { Component, OnInit, Inject } from '@angular/core';
 })
 export class CreateReferenceComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _location: Location,
+    public dialog: MatDialog
+  ) { }
 
 
   ngOnInit() {
+  }
+
+  onAddPagesClick(): boolean {
+    let dialogRef = this.dialog.open(PagesTableComponent, {
+      width: '70%'
+    });
+    return false
+  }
+
+  onAddCodesClick(): boolean {
+    let dialogRef = this.dialog.open(CodesTableComponent, {
+      width: '70%'
+    });
+    return false
   }
 
 }
