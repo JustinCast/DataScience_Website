ffc5f6b8a6abd39e461b05e690347a81a624b7cf
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-14 08:44:23 -0600

    Fixing merge omission

--- src/app/tables/thematics-table/thematics-table.component.ts
+++ src/app/tables/thematics-table/thematics-table.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit,AfterViewInit, Inject, ViewChild} from '@angular/core
 import { MatPaginator, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 import { Location } from '@angular/common';
 import { MatTableDataSource } from '@angular/material/table';
-import { ThematicToDisplay } from '../../models/ThematicToDisplay.interface';
 import {SelectionModel} from '@angular/cdk/collections';
+import { ThematicToDisplay } from '../../models/ThematicToDisplay.interface';
 
 @Component({
   selector: 'app-thematics-table',
@@ -11,6 +11,11 @@ import {SelectionModel} from '@angular/cdk/collections';
   styleUrls: ['./thematics-table.component.scss']
 })
 export class ThematicsTableComponent implements OnInit {
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   constructor(
     public dialogRef: MatDialogRef<ThematicsTableComponent>,
@@ -19,14 +24,17 @@ export class ThematicsTableComponent implements OnInit {
   onNoClick(): void {
     this.dialogRef.close();
   }
-  ngOnInit() {
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
   }
 
-  displayedColumns = ['select', 'position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
-  selection = new SelectionModel<Element>(true, []);
-
-  /** x|Whether the number of selected elements matches the total number of rows. */
+  ngOnInit() {  
+  }
+  displayedColumns = ['select', 'name', 'studyArea', 'proposal', 'blog'];
+  dataSource = new MatTableDataSource<ThematicToDisplay>(ELEMENT_DATA);
+  selection = new SelectionModel<ThematicToDisplay>(true, []);
   isAllSelected() {
     const numSelected = this.selection.selected.length;
     const numRows = this.dataSource.data.length;
@@ -39,35 +47,48 @@ export class ThematicsTableComponent implements OnInit {
         this.selection.clear() :
         this.dataSource.data.forEach(row => this.selection.select(row));
   }
-
 }
 
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+const ELEMENT_DATA:ThematicToDisplay []= [
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'algo', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'some ', studyArea: 'Hydrogen', proposal: 8, blog: 9},
+  {name:'Tematica', studyArea: 'Hydrogen', proposal: 8, blog: 9},
 ];
