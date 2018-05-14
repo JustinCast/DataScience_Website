cb7dc255473f45fecfcc375b98102ca27565ecb0
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-14 10:04:11 -0600

    References Table
    Se maquetó, diseñó y estilizó el componente ReferencesTables.component.ts

--- src/app/services/references.service.ts
+++ src/app/services/references.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Reference } from '../models/Reference';
 
 @Injectable()
 export class ReferencesService {
-
+  references: Array<Reference> = new Array()
   constructor() { }
 
-  public insertReference(){
-
+  public insertReference(ref: Reference): void {
+    this.references.unshift(ref)
   }
 
+  public getRefs(): Array<Reference> {
+    return this.references
+  }
+  
+  public getOneRef(ref_id: number): Reference {
+    let reference: Reference
+    this.references.forEach(ref => {
+      if(ref.ref_id === ref_id)
+        reference = ref
+    })
+    return reference
+  }
 }
--- src/app/tables/references-table/references-table.component.ts
+++ src/app/tables/references-table/references-table.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject, AfterViewInit } from '@angular/core';
+import { ReferencesService } from '../../services/references.service';
+import { MatPaginator, MatTableDataSource, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Reference } from '../../models/Reference';
 
 @Component({
   selector: 'app-references-table',
   templateUrl: './references-table.component.html',
-  styleUrls: ['./references-table.component.scss']
+  styleUrls: ['./references-table.component.scss'],
+  providers: [ReferencesService]
 })
-export class ReferencesTableComponent implements OnInit {
+export class ReferencesTableComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  dataSource
+  selection = new SelectionModel<Reference>(true, []);
+  addedMembers = []
+  displayedColumns = ['selection', 'ref_id', 'description', 'pages', 'codes', 'documents'];
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<ReferencesTableComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private refService: ReferencesService
+  ) { }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.refService.getRefs());
+  }
+
+  /**
+   * Set the paginator after the view init since this component will
+   * be able to query its view for the initialized paginator.
+   */
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
