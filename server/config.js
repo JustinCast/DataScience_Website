1c645b7a3c45c5e24eb28afb8fe63bcec4b781b5
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-15 11:03:14 -0600

    Primeras configuraciones del servidor

---
+++ server/config.js
@@ -0,0 +1,8 @@
+'use strict'
+module.exports = {  
+    userName: 'yourusername',  
+    password: 'yourpassword',  
+    server: 'yourserver.database.windows.net',  
+    // When you connect to Azure SQL Database, you need these next options.  
+    options: {encrypt: true, database: 'AdventureWorks'}  
+}
---
+++ server/server.js
@@ -0,0 +1,14 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+var config = require('./config')
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected');
+  }
+});
