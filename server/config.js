88945d1b201ce21ee9e0a9219115bcd9bb4414ed
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-15 21:02:15 -0600

    Enrutador Feedback y archivo conexión

--- server/config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-module.exports = {  
-    userName: 'yourusername',  
-    password: 'yourpassword',  
-    server: 'yourserver.database.windows.net',  
-    // When you connect to Azure SQL Database, you need these next options.  
-    options: {encrypt: true, database: 'AdventureWorks'}  
-}
--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-var TYPES = require('tedious').TYPES;
-var config = require('./config')
-var connection = new Connection(config);
+var express = require('express');
+var config = require('config');
+var bodyParser = require('body-parser');
+var tediousExpress = require('express4-tedious');
 
-// Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Connected');
-  }
+var app = express();
+app.use(function (req, res, next) {
+    req.sql = tediousExpress(config.get('connection'));
+    next();
 });
+
+app.use(bodyParser.text()); 
+app.use('/feedback', require('./routes/feedback'));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found: '+ req.method + ":" + req.originalUrl);
+    err.status = 404;
+    next(err);
+});
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+});
+
+module.exports = app;
