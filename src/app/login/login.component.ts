0d1e1463517619cfd33623052a6cbdbe84f3b153
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-07-17 10:11:02 -0600

    Changes before merge

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginGroup: FormGroup
+  constructor(
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit() {
   }
